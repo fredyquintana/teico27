2decca266b371a90cbfd87c1865775632a72c2fe
fredyquintana <141890829+fredyquintana@users.noreply.github.com>
2023-10-12 10:09:31 -0600

    interfaz circulo

---
+++ FiguraGeometrica/Circulo.java
@@ -0,0 +1,14 @@
+
+// Clase Círculo
+public class Circulo implements CalcularArea {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+}
