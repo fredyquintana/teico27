52f4f72c402aa34869685e5b4e932b3873562230
fergal24 <fgalindoh001@alumno.uaemex.mx>
2023-10-12 10:23:39 -0600

    corregi main

--- FiguraGeometrica/Main.java
+++ FiguraGeometrica/Main.java
@@ -3,9 +3,10 @@ public class Main {
         Rectangulo rectangulo = new Rectangulo(5, 10);
         Triangulo triangulo = new Triangulo(8, 6);
         Circulo circulo = new Circulo(4);
-
+        System.out.println("----------------------------------------------------");
         System.out.println("Área del Rectángulo: " + rectangulo.calcularArea());
         System.out.println("Área del Triángulo: " + triangulo.calcularArea());
         System.out.println("Área del Círculo: " + circulo.calcularArea());
+        System.out.println("----------------------------------------------------");
     }
 }
