acd12fff74defa348a077cf9154b67be7d3d6852
fredyquintana <141890829+fredyquintana@users.noreply.github.com>
2023-10-13 14:02:05 -0600

    Cambios

--- FiguraGeometrica/Main.java
+++ FiguraGeometrica/Main.java
@@ -3,10 +3,17 @@ public class Main {
         Rectangulo rectangulo = new Rectangulo(5, 10);
         Triangulo triangulo = new Triangulo(8, 6);
         Circulo circulo = new Circulo(4);
+        Pentagono pentagono = new Pentagono(20, 7);
+        Rombo rombo = new Rombo(6, 8);
+        Trapecio trapecio = new Trapecio(5, 10, 6);
+        
         System.out.println("----------------------------------------------------");
         System.out.println("Área del Rectángulo: " + rectangulo.calcularArea());
         System.out.println("Área del Triángulo: " + triangulo.calcularArea());
         System.out.println("Área del Círculo: " + circulo.calcularArea());
+        System.out.println("Área del Pentágono: " + pentagono.calcularArea());
+        System.out.println("Área del Rombo: " + rombo.calcularArea());
+        System.out.println("Área del Trapecio: " + trapecio.calcularArea());
         System.out.println("----------------------------------------------------");
     }
 }
